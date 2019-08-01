6ce0ef4739159a1d04bcbdda8b13e24bb08670d9
Administrador <Administrador@10.10.4.12>
2019-08-01 16:14:42 -0300

    End

---
+++ endereco/src/dao/EnderecoDao.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dao;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+import modelo.Endereco;
+/**
+ *
+ * @author Administrador
+ */
+public class EnderecoDao {
+    public static boolean inserir(Endereco objeto) {
+        String sql = "INSERT INTO endereco (bairro, longadouro, complemento) VALUES (?, ?, ?)";
+        try {
+            PreparedStatement ps = conexao.Conexao.getConexao().prepareStatement(sql);
+            ps.setString(1, objeto.getBairro());
+            ps.setString(2, objeto.getLongadouro());
+            ps.setString(3, objeto.getComplemento());
+            ps.executeUpdate();
+            return true;
+        } catch (SQLException | ClassNotFoundException ex) {
+            System.out.println(ex.getMessage());
+            return false;
+        }
+    }
+    
+    public static void main(String[] args) {
+        Endereco objeto = new Endereco();
+        objeto.setBairro("Planalto");
+        objeto.setLongadouro("Av. Brasil");
+        objeto.setComplemento("1000");
+        
+        boolean resultado = inserir(objeto);
+        if (resultado) {
+            JOptionPane.showMessageDialog(null, "Inserido com sucesso!");
+        } else {
+            JOptionPane.showMessageDialog(null, "Erro!");
+        }
+    }
+}
---
+++ endereco/src/modelo/Endereco.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+
+/**
+ *
+ * @author Administrador
+ */
+public class Endereco {
+    private int codigo;
+    private String bairro;
+    private String longadouro;
+    private String complemento;
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getLongadouro() {
+        return longadouro;
+    }
+
+    public void setLongadouro(String longadouro) {
+        this.longadouro = longadouro;
+    }
+
+    public String getComplemento() {
+        return complemento;
+    }
+
+    public void setComplemento(String complemento) {
+        this.complemento = complemento;
+    }
+
+    @Override
+    public String toString() {
+        return longadouro;
+    }
+    
+}
